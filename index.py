99bab29166ac3c7cf9f73e734ad3e1ac6f191af9
GBJ <gabriel@bialy.ca>
2022-11-13 15:54:05 +0100

    Hopefully added killing of the nohup process for the nas

--- index.py
+++ index.py
@@ -188,6 +188,10 @@ async def breadmanage(interaction: discord.Interaction, reason: str, type: str):
             f.write(f"\nBread bot has been updated for the reason {reason} \n Command has been run at: " + time.ctime())
             f.close()
         os.system("git pull https://github.com/WhineyMonkey10/Bread-bot")
+        with open('pid.txt', "r") as f:
+            pid = f.read()
+            f.close()
+        os.system(f"kill {pid}")
         os.system("nohup python index.py &")
         
 
