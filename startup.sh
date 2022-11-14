3bfbe39eb340fd26b7a294f2c1d45b1f7da3320b
GBJ <gabriel@bialy.ca>
2022-11-14 17:54:41 +0100

    New inf loop on startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,19 @@
 echo "Script started at $(date)"
-if [ ps -p $(cat $HOME/bread-bot/pid.txt) ]; then
-    echo "Bot is running"
-    echo "Bot is running at $(date)" >> $HOME/bread-bot/restart.log
-else
-    echo "Bot is not running"
-    echo "Restarting bot..."
-    cd $HOME/bread-bot
-    nohup python index.py &
-    echo $! > $HOME/bread-bot/pid.txt
-    echo "Bot restarted at $(date)" >> $HOME/bread-bot/restart.log
-fi
+
+while true; do
+    if [ ps -p $(cat $HOME/bread-bot/pid.txt) ]; then
+        echo "Bot is running"
+        echo "Bot is running at $(date)" >> $HOME/bread-bot/restart.log
+    else
+        echo "Bot is not running"
+        echo "Restarting bot..."
+        cd $HOME/bread-bot
+        nohup python index.py &
+        echo $! > $HOME/bread-bot/pid.txt
+        echo "Bot restarted at $(date)" >> $HOME/bread-bot/restart.log
+    fi
+    sleep 1
+done
+
 
 
