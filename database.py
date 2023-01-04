f72a62dc33fb3a63b6033a848c9b27d432942618
GBJ <gbialystokjoly@gmail.com>
2023-01-04 10:06:09 +0100

    Fix for remove?

--- database.py
+++ database.py
@@ -44,7 +44,7 @@ class Database:
             return "User does not exist, please use the command `/start` to get started!"
 
     def deleteaccount(user_id):
-        collection.delete_one({"user_id": user_id})
+        collection.delete_many({"user_id": user_id})
         return "Account deleted!"
     
     def adduser(user_id):
